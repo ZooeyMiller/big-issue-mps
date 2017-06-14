902b64428fbcb6ea360f47600666381df7b41609
finnhodgkin <finnhodgkin@gmail.com>
2017-06-14 22:47:58 +0100

    Adds database functions to add user message and check user by id.       Also RETURNS ID where possible #3

--- src/utils/database.js
+++ src/utils/database.js
@@ -11,10 +11,30 @@ const query = (query, valuesArray) => {
 
 const insertUserInfoRow = ({ uuid, name, email, mpName, mpEmail }) => {
   return new Promise((resolve, reject) => {
-    const insertionQuery = `INSERT INTO emails (uuid, name, email, mp_name, mp_email, sent) VALUES ($1, $2, $3, $4, $5, $6)`;
+    const insertionQuery = `INSERT INTO emails (uuid, name, email, mp_name, mp_email, sent) VALUES ($1, $2, $3, $4, $5, $6) RETURNING ID`;
     const insertionValues = [uuid, name, email, mpName, mpEmail, false];
-    query(insertionQuery, insertionValues).then(resolve).catch(reject);
+    query(insertionQuery, insertionValues)
+      .then(res => resolve(res.rows[0].id))
+      .catch(reject);
   });
 };
 
-module.exports = { insertUserInfoRow };
+const getUserById = id => {
+  return new Promise((resolve, reject) => {
+    const getQuery = `SELECT uuid, name, email, mp_name, mp_email, sent, user_input FROM emails WHERE id = $1`;
+    const getValue = [id];
+    query(getQuery, getValue).then(res => resolve(res.rows[0])).catch(reject);
+  });
+};
+
+const updateUserMessage = (id, userMessage) => {
+  return new Promise((resolve, reject) => {
+    const insertionQuery = `UPDATE emails SET user_input = $1 WHERE id = $2 RETURNING ID`;
+    const insertionValue = [userMessage, id];
+    query(insertionQuery, insertionValue)
+      .then(res => resolve(res.rows[0].id))
+      .catch(reject);
+  });
+};
+
+module.exports = { insertUserInfoRow, getUserById, updateUserMessage };
